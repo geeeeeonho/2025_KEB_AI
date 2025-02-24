e8ce7a30009f96581673c4068d1840210d41d0fc
이건호 <ggamea100@gmail.com>
2025-02-24 16:37:19 +0900

    v0.5 day01 selfmachine

--- day11.py
+++ day11.py
@@ -1,24 +1,21 @@
-import matplotlib.pyplot as plt                   #그래픽 라이브러리
-import pandas as pd                               #판다스
-from sklearn.linear_model import LinearRegression #모델 불러오기
-from sklearn.neighbors import KNeighborsRegressor
+# from sklearn.linear_model import LinearRegression
+# import ghlearn as gh
+from ghlearn import LinearRegression #자체 제작 예측
+import pandas as pd
+import matplotlib.pyplot as plt
 
-#read_csv : pandas에서 기본 제공하는 읽기
-#데이터 해당 링크의 데이터 읽어오기
 ls = pd.read_csv("https://github.com/ageron/data/raw/main/lifesat/lifesat.csv")
-#print(ls)
-X=ls[["GDP per capita (USD)"]].values   #x축 설정
-Y=ls[["Life satisfaction"]].values        #y축 설정
+X = ls[["GDP per capita (USD)"]].values
+y = ls[["Life satisfaction"]].values
 
-#데이터를 그래프로 나타냄
-ls.plot(kind='scatter',grid=True,
-             x="GDP per capita (USD)", y="Life satisfaction")
-plt.axis([23500, 62500, 4, 9]) #axis([x최소범위,x최대범위,y최소범위,y최대범위])
-plt.show() #그래프를 보여줌
+ls.plot(kind='scatter', grid=True, x="GDP per capita (USD)", y="Life satisfaction")
+plt.axis([23500, 62500, 4, 9])
+plt.show()
 
-#model = LinearRegression() #선형 회귀 모델 제작
-model = KNeighborsRegressor(n_neighbors=3)  #최근접 이웃 회귀 모델 제작
-model.fit(X,Y)
+model = LinearRegression()
+model.fit(X, y)
 
-X_new = [[37655.2]]
-print('37655.2->',model.predict(X_new)) #predict(x) x를 이용한 예측모델
+X_new = [[31721.3]]  # ROK 2020
+print(model.predict(X_new))
+# LinearRegression 5.90
+# KNeighborsRegressor 5.70
