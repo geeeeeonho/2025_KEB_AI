d283688fbf67111acc21ddf4b4ce1da5a22db810
이건호 <ggamea100@gmail.com>
2025-02-25 16:20:20 +0900

    v1.1 day11_HW base

--- day12.py
+++ day12.py
@@ -3,10 +3,11 @@ import pandas as pd #판다스
 from sklearn.impute import SimpleImputer
 
 #임의로 결측치를 만들기
+#결측치 값을 산술평균으로 채워넣는 다양한 방법을 적용
 df = pd.DataFrame(
     {
         'A':[1 , 2 , np.nan , 4],
-        'B':[np.nan , 2 , 3 , 4],
+        'B':[np.nan , 12 , 3 , 4],
         'C':[1 , 2 , 3 , 4],
     }
 )
