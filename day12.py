606d6f12175ab68c86d6a3053314dc47d0665a5a
이건호 <ggamea100@gmail.com>
2025-02-25 16:45:20 +0900

    v1.2 day11_HW AS2

--- day12.py
+++ day12.py
@@ -4,7 +4,7 @@ from sklearn.impute import SimpleImputer
 
 #임의로 결측치를 만들기
 #결측치 값을 산술평균으로 채워넣는 다양한 방법을 적용
-df = pd.DataFrame(
+df1 = pd.DataFrame(
     {
         'A':[1 , 2 , np.nan , 4],
         'B':[np.nan , 12 , 3 , 4],
@@ -18,8 +18,14 @@ df = pd.DataFrame(
 2  NaN   3.0  3
 3  4.0   4.0  4
 """
-
+df=df1.copy()
 #1.imputer 사용 - 평균으로 채우기
 i = SimpleImputer(strategy='mean')  #평균으로 채우기
 df[['A','B']] = i.fit_transform(df[['A','B']])
-print(df)
+print('imputer 사용\n',df)
+
+df=df1.copy()
+#2. 산술평균 사용
+i = df[['A','B']].mean() #A,B 열의 산술 평균 계산
+df[['A','B']] = df[['A','B']].fillna(i) #그 평균값을 원래의 데이터 프레임에 적용
+print('산술평균 사용\n',df)
